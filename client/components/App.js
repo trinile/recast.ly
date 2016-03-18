256921c1490ffd3b907c88e4228b013a38fbbfa9
trinile <trinile@gmail.com>
2016-03-18 15:36:48 -0700

    Create a dynamic video component

--- client/components/App.js
+++ client/components/App.js
@@ -2,7 +2,7 @@ var App = () => (
   <div>
     <Nav />
     <div className="col-md-7">
-      <VideoPlayer/>
+      <VideoPlayer currentVideo={exampleVideoData[0]} />
     </div>
     <div className="col-md-5">
       <VideoList videos={ exampleVideoData }/>
--- client/components/VideoPlayer.js
+++ client/components/VideoPlayer.js
@@ -1,11 +1,11 @@
-var VideoPlayer = () => (
+var VideoPlayer = (props) => (
   <div className="video-player">
     <div className="embed-responsive embed-responsive-16by9">
-      <iframe className="embed-responsive-item" src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1" allowFullScreen></iframe>
+      <iframe className="embed-responsive-item" src={`https://www.youtube.com/embed/${props.currentVideo.id.videoId}?autoplay=1`}  allowFullScreen></iframe>
     </div>
     <div className="video-player-details">
-      <h3>Video Title</h3>
-      <div>Video Description</div>
+      <h3>{props.currentVideo.snippet.title}</h3>
+      <div>{props.currentVideo.snippet.desc}</div>
     </div>
   </div>
 );
